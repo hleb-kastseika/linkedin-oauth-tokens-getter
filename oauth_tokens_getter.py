ef811acf9b305415d23ec18b06268634042f588a
Gleb Kosteiko <gleb.kosteiko@gmail.com>
2018-08-31 10:54:23 +0300

    Fix codestyle

--- oauth_tokens_getter.py
+++ oauth_tokens_getter.py
@@ -7,18 +7,18 @@ consumer = oauth.Consumer(consumer_key, consumer_secret)
 client = oauth.Client(consumer)
 
 request_token_url = 'https://api.linkedin.com/uas/oauth/requestToken'
-resp, content = client.request(request_token_url, "POST")
-if resp['status'] != '200' :
+resp, content = client.request(request_token_url, 'POST')
+if resp['status'] is not '200' :
     raise Exception('Invalid response %s.' % resp['status'])
-content_utf8 = str(content,'utf-8')
+content_utf8 = str(content, 'utf-8')
 request_token = dict(urllib.parse.parse_qsl(content_utf8))
 authorize_url = request_token['xoauth_request_auth_url']
 
-print('Go to the following link in your browser:', "\n")
+print('Go to the following link in your browser:', '\n')
 print(authorize_url + '?oauth_token=' + request_token['oauth_token'])
 
-accepted='n'
-while accepted.lower() == 'n' :
+accepted = 'n'
+while accepted.lower() is 'n' :
     accepted = input('Have you authorized me? (y/n)')
 oauth_verifier = input('What is the PIN?')
 
@@ -30,7 +30,7 @@ resp, content = client.request(access_token_url, 'POST')
 content8 = str(content,'utf-8')
 access_token = dict(urllib.parse.parse_qsl(content8))
 
-print('Access Token:', "\n")
-print('- oauth_token        = ' + access_token['oauth_token']+'\n')
+print('Access Token:', '\n')
+print('- oauth_token        = ' + access_token['oauth_token'] + '\n')
 print('- oauth_token_secret = ' + access_token['oauth_token_secret'])
 print('You may now access protected resources using the access tokens above.')
